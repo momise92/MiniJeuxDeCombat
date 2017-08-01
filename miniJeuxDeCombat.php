3aaf92d37478513b57263add334bea84a5d9aeea
momise92 <boss973@hotmail.fr>
2017-08-01 04:43:19 +0200

    Création des methodes de la classe

--- miniJeuxDeCombat.php
+++ miniJeuxDeCombat.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Titre Doc
+ * Personnage
  *
  * PHP Version 7.1
  *
@@ -15,4 +15,20 @@ class Personnage
     private $_id,
     $_degats,
     $_nom;
+
+    public function frapper(Personnage $perso)
+    {
+        // Avant tout : vérifier qu'on ne se frappe pas soi-même.
+        // Si c'est le cas, on stoppe tout en renvoyant une valeur signifiant que le personnage ciblé est le personnage qui attaque.
+        // On indique au personnage frappé qu'il doit recevoir des dégâts.
+    }
+
+    public function recevoirDegats()
+    {
+        // On augmente de 5 les dégâts.
+        
+        // Si on a 100 de dégâts ou plus, la méthode renverra une valeur signifiant que le personnage a été tué.
+        
+        // Sinon, elle renverra une valeur signifiant que le personnage a bien été frappé.
+    }
 }
