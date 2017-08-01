c5ed76c5503a29f29caf0488182327c0fcf09fc6
momise92 <boss973@hotmail.fr>
2017-08-01 04:39:52 +0200

    Création des attributs de la classe personnage

---
+++ miniJeuxDeCombat.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Titre Doc
+ *
+ * PHP Version 7.1
+ *
+ * @category Categorie
+ * @package  Package
+ * @author   Hash Carter <coulang43@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/momise92/MiniJeuxDeCombat
+ */
+class Personnage
+{
+    private $_id,
+    $_degats,
+    $_nom;
+}
